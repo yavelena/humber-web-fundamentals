132ca005246efc1e8eb1a9e08379c652498f9972
Olena Ieshchenko <yavelena@gmail.com>
2026-04-05 22:09:33 -0400

    added fade-in effect

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
+/****** FADE IN EFFECT ********/
+
+const fadeItems = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(
+    (entries) =>
+    {
+        entries.forEach((entry) =>
+        {
+            if (entry.isIntersecting)
+            {
+                entry.target.classList.add("is-visible");
+                observer.unobserve(entry.target);
+            }
+        });
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+function attachFadeObserver()
+{
+    const fadeItems = document.querySelectorAll(".fade-in:not(.is-visible)");
+
+    fadeItems.forEach((item) =>
+    {
+        observer.observe(item);
+    });
+}
+
+
+
+/****** PROJECTS SEARCH ********/
+
 const projectsContainer = document.querySelector("#projects-container");
 
 const searchInput = document.querySelector("#project-search-input");
@@ -8,8 +43,8 @@ function createProjectCard(project) {
     const techsHtml = project.techs.map(tech => `<span>${tech}</span>`).join("");
 
     return `
-        <article class="project-card flex-col">
-            <img src="${project.image}" alt="${project.alt}" />
+        <article class="project-card flex-col fade-in" style="--delay: 0.12}s;">
+            <img src="${project.image}" alt="${project.alt}" class="fade-in"/>
             <h3>${project.title}</h3>
             <p>${project.description}</p>
             <p class="project-techs">${techsHtml}</p>
@@ -21,12 +56,17 @@ function createProjectCard(project) {
 
 
 function renderProjects(projects) {
+
     if (projects.length === 0) {
-        projectsContainer.innerHTML = `<p class="no-projects">No projects found.</p>`;
+        projectsContainer.innerHTML = `
+            <p><b>No matching projects found.</b><br/>
+            Try another keyword or reset the search to view the full repository.</p>
+        `;
         return;
     }
 
     projectsContainer.innerHTML = projects.map(createProjectCard).join("");
+    attachFadeObserver();
 }
 
 
@@ -64,3 +104,6 @@ searchInput.addEventListener("input", Search);
 searchButton.addEventListener("click", Search);
 
 renderProjects(myProjects);
+
+
+
